e44dfb3c9a326c6521cb5560ec038e54d82c32b7
Gani <30381081+misagani@users.noreply.github.com>
2018-01-12 07:38:57 +0700

    Add files via upload

---
+++ sources/EnryptDecryptDudi265/MainForm.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace EnryptDecryptDudi265
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblPlain = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblEncrypt = new System.Windows.Forms.Label();
+            this.lblDecrypt = new System.Windows.Forms.Label();
+            this.btnProcess = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblPlain
+            // 
+            this.lblPlain.Location = new System.Drawing.Point(221, 39);
+            this.lblPlain.Name = "lblPlain";
+            this.lblPlain.Size = new System.Drawing.Size(322, 31);
+            this.lblPlain.TabIndex = 0;
+            this.lblPlain.Text = "lorem ipsum dolor sit amet";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "PlainText";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(45, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(127, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "EncryptText";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(45, 274);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(128, 25);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "DecryptText";
+            // 
+            // lblEncrypt
+            // 
+            this.lblEncrypt.Location = new System.Drawing.Point(216, 99);
+            this.lblEncrypt.Name = "lblEncrypt";
+            this.lblEncrypt.Size = new System.Drawing.Size(391, 146);
+            this.lblEncrypt.TabIndex = 4;
+            this.lblEncrypt.Text = "[EncryptText - Result]";
+            // 
+            // lblDecrypt
+            // 
+            this.lblDecrypt.AutoSize = true;
+            this.lblDecrypt.Location = new System.Drawing.Point(216, 274);
+            this.lblDecrypt.Name = "lblDecrypt";
+            this.lblDecrypt.Size = new System.Drawing.Size(220, 25);
+            this.lblDecrypt.TabIndex = 5;
+            this.lblDecrypt.Text = "[DecryptText - Result]";
+            // 
+            // btnProcess
+            // 
+            this.btnProcess.Location = new System.Drawing.Point(221, 368);
+            this.btnProcess.Name = "btnProcess";
+            this.btnProcess.Size = new System.Drawing.Size(172, 50);
+            this.btnProcess.TabIndex = 6;
+            this.btnProcess.Text = "Process";
+            this.btnProcess.UseVisualStyleBackColor = true;
+            this.btnProcess.Click += new System.EventHandler(this.btnProcess_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(619, 449);
+            this.Controls.Add(this.btnProcess);
+            this.Controls.Add(this.lblDecrypt);
+            this.Controls.Add(this.lblEncrypt);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblPlain);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Dudi265 - StringCrypto v1.0";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox lblPlain;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblEncrypt;
+        private System.Windows.Forms.Label lblDecrypt;
+        private System.Windows.Forms.Button btnProcess;
+    }
+}
+
---
+++ sources/EnryptDecryptDudi265/MainForm.cs
@@ -0,0 +1,46 @@
+﻿// Dudi265 v1.0 [Example Usage]
+// By        : VickyAziz
+// Version   : 1.0
+// Release   :
+//  --> v1.0 (13/10/2017)
+//      - First Release "Dudi265 - StringCrypto" 
+//      - Simple UI, proof of concept algorithm
+//      - Can only encrypt-decrypt string
+//      - Example Usage
+
+// Library Used
+using System;
+using System.Windows.Forms;
+using Dudi265Lib; // add this library
+
+namespace EnryptDecryptDudi265
+{
+    public partial class MainForm : Form
+    {
+        // Main Form
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        // Button Pressed
+        private void btnProcess_Click(object sender, EventArgs e)
+        {
+            // Local Variables
+            string tempPlain = "";
+            string tempEncrypt = "";
+            string tempDecrypt = "";
+
+            // Process Grab Text from TextForm
+            tempPlain = lblPlain.Text;
+            // Process Encryption
+            tempEncrypt = Dudi265.Encrypt(tempPlain);
+            // Process Decryption
+            tempDecrypt = Dudi265.Decrypt(tempEncrypt);
+
+            // Display Label
+            lblEncrypt.Text = tempEncrypt;
+            lblDecrypt.Text = tempDecrypt;
+        }
+    }
+}
